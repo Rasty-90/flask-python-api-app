2c303eafc4adf27143e89c08ff82710f10f237da
Rasty-90 <as2paine@gmail.com>
2020-10-23 13:32:31 +0300

    Update app.py
    added basic routing

--- app.py
+++ app.py
@@ -1,36 +1,42 @@
-from flask import Flask,request,Response
-from database.dp import initialize_db
-from database.models import Movie
+from flask import Flask,request,Response,render_template
+# from database.dp import initialize_db
+# from database.models import Movie
 
 
 app = Flask(__name__)
 
-app.config['MONGODB_SETTINGS'] = {
-    'host': 'mongodb://localhost/movie-bag'
-}
+    # app.config['MONGODB_SETTINGS'] = {
+    #     'host': 'mongodb://localhost/movie-bag'
+    # }
 
-initialize_db(app)
+    # initialize_db(app)
 
-@app.route('/movies')
-def get_movies():
-    movies = Movie.objects().to_json()
-    return Response(movies, mimetype="application/json", status=200)
+@app.route('/home')
+def check_home():
+    return render_template('home.html', title='Home')
 
-@app.route('/movies', methods=['POST'])
-    body = request.get_json()
-    movie = Movie(**body).save()
-    id = movie.id
-    return {'id': str(id)}, 200
+@app.route('/about')
+def check_about():
+    return render_template('about.html', title='about')
+    # def get_movies():
+    #     movies = Movie.objects().to_json()
+    #     return Response(movies, mimetype="application/json", status=200)
 
-@app.route('/movies/<id>', methods=['PUT'])
-def update_movie(id):
-    body = request.get_json()
-    Movie.objects.get(id=id).update(**body)
-    return '', 200
+    # @app.route('/movies', methods=['POST'])
+    #     body = request.get_json()
+    #     movie = Movie(**body).save()
+    #     id = movie.id
+    #     return {'id': str(id)}, 200
 
-@app.route('/movies/<id>')
-def get_movie(id):
-    movies = Movie.objects.get(id=id).to_json()
-    return Response(movies, mimetype="application/json", status=200)
+    # @app.route('/movies/<id>', methods=['PUT'])
+    # def update_movie(id):
+    #     body = request.get_json()
+    #     Movie.objects.get(id=id).update(**body)
+    #     return '', 200
+
+    # @app.route('/movies/<id>')
+    # def get_movie(id):
+    #     movies = Movie.objects.get(id=id).to_json()
+    #     return Response(movies, mimetype="application/json", status=200)
 
 app.run()
