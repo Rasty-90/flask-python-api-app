564968b36b404560b4fc27f89eec1e7db83334b9
Rasty-90 <as2paine@gmail.com>
2020-10-23 10:33:53 +0300

    +g

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask,request,Response
+from database.dp import initialize_db
+from database.models import Movie
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return {'hello': 'world'}
+app.config['MONGODB_SETTINGS'] = {
+    'host': 'mongodb://localhost/movie-bag'
+}
 
+initialize_db(app)
+
+@app.route('/movies')
+def get_movies():
+    movies = Movie.objects().to_json()
+    return Response(movies, mimetype="application/json", status=200)
+
+@app.route('/movies', methods=['POST'])
+    body = request.get_json()
+    movie = Movie(**body).save()
+    id = movie.id
+    return {'id': str(id)}, 200
+
+@app.route('/movies/<id>', methods=['PUT'])
+def update_movie(id):
+    body = request.get_json()
+    Movie.objects.get(id=id).update(**body)
+    return '', 200
+
+@app.route('/movies/<id>')
+def get_movie(id):
+    movies = Movie.objects.get(id=id).to_json()
+    return Response(movies, mimetype="application/json", status=200)
 
 app.run()
---
+++ database/models.py
@@ -0,0 +1,6 @@
+from .dp import db
+
+class Movie(db.Document):
+    name = db.StringField(required=True, unique=True)
+    casts = db.ListField(db.StringField(), required=True)
+    genres = db.ListField(db.StringField(), required=True)
